bc8cd2e85a182b4a1ec7fde5069372a1c66cc5d6
jmseb3 <jmseb3@naver.com>
2023-04-26 14:27:44 +0900

    LoginViews move to ui.login

---
+++ androidApp/src/main/java/com/wonddak/loacell/android/ui/login/LoginInfoView.kt
@@ -0,0 +1,41 @@
+package com.wonddak.loacell.android.ui.login
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.google.firebase.auth.FirebaseUser
+import com.wonddak.loacell.android.util.LoginHelper
+
+@Composable
+fun LoginInfoView(
+    user : FirebaseUser?,
+    loginHelper: LoginHelper
+) {
+    val anonymousToGoogleLoginLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        loginHelper.registerAnonymousToGoogle(result)
+    }
+    AnimatedVisibility(visible = user!= null) {
+        Column() {
+            Text(text = user?.email.toString())
+            Text(text = user?.uid.toString())
+            if (user?.isAnonymous == true) {
+                OutlinedButton(onClick = {
+                    loginHelper.requestGoogleLogin {
+                        anonymousToGoogleLoginLauncher.launch(it)
+                    }
+                }) {
+                    Text(text = "Google과 연동")
+                }
+            }
+            OutlinedButton(onClick = { loginHelper.signOut() }) {
+                Text(text = "LogOut")
+            }
+        }
+    }
+}
--- androidApp/src/main/java/com/wonddak/loacell/android/ui/login/LoginView.kt
+++ androidApp/src/main/java/com/wonddak/loacell/android/ui/login/LoginView.kt
@@ -1,8 +1,7 @@
-package com.wonddak.loacell.android
+package com.wonddak.loacell.android.ui.login
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,7 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.firebase.auth.FirebaseUser
+import com.wonddak.loacell.android.R
 import com.wonddak.loacell.android.util.LoginHelper
 
 @Composable
@@ -101,34 +99,4 @@ fun LoginButton(
         content()
     }
 
-}
-
-@Composable
-fun LoginInfoView(
-    user : FirebaseUser?,
-    loginHelper: LoginHelper
-) {
-    val anonymousToGoogleLoginLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartIntentSenderForResult()
-    ) { result ->
-        loginHelper.registerAnonymousToGoogle(result)
-    }
-    AnimatedVisibility(visible = user!= null) {
-        Column() {
-            Text(text = user?.email.toString())
-            Text(text = user?.uid.toString())
-            if (user?.isAnonymous == true) {
-                OutlinedButton(onClick = {
-                    loginHelper.requestGoogleLogin {
-                        anonymousToGoogleLoginLauncher.launch(it)
-                    }
-                }) {
-                    Text(text = "Google과 연동")
-                }
-            }
-            OutlinedButton(onClick = { loginHelper.signOut() }) {
-                Text(text = "LogOut")
-            }
-        }
-    }
 }
