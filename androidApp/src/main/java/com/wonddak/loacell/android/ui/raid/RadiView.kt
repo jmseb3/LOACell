5f6e17aadd11fc8da0e99550238b698f0915acd8
jmseb3 <jmseb3@naver.com>
2023-05-08 17:32:48 +0900

    캐릭터 정보 업데이트 추가

--- androidApp/src/main/java/com/wonddak/loacell/android/ui/raid/RadiView.kt
+++ androidApp/src/main/java/com/wonddak/loacell/android/ui/raid/RadiView.kt
@@ -2,10 +2,17 @@ package com.wonddak.loacell.android.ui.raid
 
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Tab
@@ -16,18 +23,26 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.zIndex
 import com.holix.android.bottomsheetdialog.compose.BottomSheetDialog
 import com.holix.android.bottomsheetdialog.compose.BottomSheetDialogProperties
 import com.wonddak.loacell.RaidInfo
+import com.wonddak.loacell.SharedRes
 import com.wonddak.loacell.android.ui.bottomSheet.AddRaidSheet
 import com.wonddak.loacell.android.ui.bottomSheet.BaseSheet
+import com.wonddak.loacell.android.ui.common.MyIconButton
 import com.wonddak.loacell.android.ui.raid.user.AddUserView
 import com.wonddak.loacell.android.ui.raid.user.UserInfoCard
 import com.wonddak.loacell.database.AppDataBase
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @Composable
 fun RaidView(
@@ -35,6 +50,7 @@ fun RaidView(
     selectedRoomId: Long,
     backAction: () -> Unit
 ) {
+    val scope = rememberCoroutineScope()
     val roomInfo = db.roomInfoQueriesHelper.getRoomInfoById(selectedRoomId)
     val raidInfoList by db.raidInfoQueriesHelper.getALlByRoomId(selectedRoomId)
         .collectAsState(initial = emptyList())
@@ -47,60 +63,115 @@ fun RaidView(
     var showAddUserSheet by remember {
         mutableStateOf(false)
     }
+
+    var showLoadingProgress by remember {
+        mutableStateOf(false)
+    }
+    var showLoadingProgressText by remember {
+        mutableStateOf("")
+    }
     BackHandler(!showAddUserSheet && !showAddRaidSheet) {
         backAction()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .zIndex(0f)
-    ) {
-        OutlinedButton(onClick = { backAction() }) {
-            Text(text = "BACK")
-        }
+    Box {
+        if (showLoadingProgress) {
+            val interactionSource = MutableInteractionSource()
 
-        Text(text = "SelectRoomId : ${roomInfo.id}")
-        Text(text = "title : ${roomInfo.title}")
-        Text(text = "description : ${roomInfo.description}")
-        Divider()
-        var tabState by remember { mutableStateOf(0) }
-        val titles = listOf("Raid", "User")
-        TabRow(selectedTabIndex = tabState) {
-            titles.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(title) },
-                    selected = tabState == index,
-                    onClick = { tabState = index }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Gray.copy(0.6f))
+                    .clickable(
+                        interactionSource = interactionSource,
+                        indication = null
+                    ) {
+
+                    }
+                    .zIndex(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
                 )
+                if (showLoadingProgressText.isNotEmpty()) {
+                    Text(text = "${showLoadingProgressText}님의 정보를 업데이트 중입니다.")
+                }
             }
         }
-        when (tabState) {
-            0 -> {
-                OutlinedButton(onClick = { showAddRaidSheet = true }) {
-                    Text(text = "show")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .zIndex(0f)
+        ) {
+            OutlinedButton(onClick = { backAction() }) {
+                Text(text = "BACK")
+            }
+
+            Text(text = "SelectRoomId : ${roomInfo.id}")
+            Text(text = "title : ${roomInfo.title}")
+            Text(text = "description : ${roomInfo.description}")
+            Divider()
+            var tabState by remember { mutableStateOf(0) }
+            val titles = listOf("Raid", "User")
+            TabRow(selectedTabIndex = tabState) {
+                titles.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = tabState == index,
+                        onClick = { tabState = index }
+                    )
                 }
-                LazyColumn {
-                    items(raidInfoList) { raidInfo ->
-                        RaidItemRow(raidInfo)
+            }
+            when (tabState) {
+                0 -> {
+                    MyIconButton(
+                        SharedRes.images.add
+                    ) {
+                        showAddRaidSheet = true
+                    }
+                    LazyColumn {
+                        items(raidInfoList) { raidInfo ->
+                            RaidItemRow(raidInfo)
+                        }
                     }
                 }
-            }
 
-            1 -> {
-                OutlinedButton(onClick = { showAddUserSheet = true }) {
-                    Text(text = "show")
-                }
-                val userList by db.getUsersByRoomId(roomInfo.id)
-                    .collectAsState(initial = emptyList())
-                LazyColumn {
-                    items(userList) { user ->
-                        UserInfoCard(db = db, user = user)
+                1 -> {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        MyIconButton(
+                            SharedRes.images.add
+                        ) {
+                            showAddUserSheet = true
+                        }
+                        MyIconButton(
+                            SharedRes.images.refresh
+                        ) {
+                            scope.launch {
+                                showLoadingProgress = true
+                                db.updateUserCharacters(roomId = roomInfo.id).collect {
+                                    showLoadingProgressText = it
+                                    // TODO update Last update Time
+                                }
+                                showLoadingProgress = false
+                            }
+                        }
+                    }
+                    val userList by db.getUsersByRoomId(roomInfo.id)
+                        .collectAsState(initial = emptyList())
+                    LazyColumn {
+                        items(userList) { user ->
+                            UserInfoCard(db = db, user = user)
+                        }
                     }
                 }
             }
         }
     }
+
 
     if (showAddRaidSheet) {
         BackHandler(true) {
--- shared/src/commonMain/kotlin/com/wonddak/loacell/database/AppDataBase.kt
+++ shared/src/commonMain/kotlin/com/wonddak/loacell/database/AppDataBase.kt
@@ -13,7 +13,9 @@ import com.wonddak.loacell.database.const.RaidType
 import com.wonddak.loacell.database.queriesHelper.RaidInfoQueriesHelper
 import com.wonddak.loacell.database.queriesHelper.RoomInfoQueriesHelper
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.transform
 
 class AppDataBase(driverFactory: DriverFactory) {
@@ -109,6 +111,27 @@ class AppDataBase(driverFactory: DriverFactory) {
             .mapToList(Dispatchers.Main).transform {
                 emit(it.sortedByDescending { it.level.replace(",", "").toFloat()  })
             }
+    }
+
+    suspend fun updateUserCharacters(roomId: Long) : Flow<String> = flow{
+        database.userInfoQueries.selectByRoomId(roomId).executeAsList().forEach { userInfo ->
+            val characterList = api.getCharacterInfo(userInfo.representativeCharacter)
+            emit(userInfo.name)
+            database.transaction {
+                characterList.forEach { characterInfo ->
+                    database.characterQueries.insertCharacterInfo(
+                        characterInfo.characterName,
+                        userInfo.userId,
+                        characterInfo.serverName,
+                        characterInfo.characterClassName,
+                        characterInfo.itemMaxLevel
+                    )
+                }
+            }
+            delay(500)
+        }
+        delay(1_000)
+        emit("finish")
     }
 
 }
