47610412c6ce21642ba83ea7ea98666978439b5e
jmseb3 <jmseb3@naver.com>
2023-06-23 11:20:42 +0900

    android 아이콘 설정 및 메인화면에서 두번 눌러 앱 종료

--- androidApp/src/main/java/com/wonddak/loacell/android/MainActivity.kt
+++ androidApp/src/main/java/com/wonddak/loacell/android/MainActivity.kt
@@ -2,7 +2,10 @@ package com.wonddak.loacell.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.wonddak.database.AppDataBase
@@ -16,19 +19,30 @@ import com.wonddak.loacell.android.viewModel.LoaCellViewModelFactory
 class MainActivity : ComponentActivity() {
     private lateinit var loginHelper: LoginHelper
     private lateinit var loaCellViewModel: LoaCellViewModel
+    private var waitTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginHelper = LoginHelper(this)
         val db = AppDataBase(DriverFactory(this))
         val config = Config(this)
+
         FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
 
         loaCellViewModel = ViewModelProvider(
             this,
-            LoaCellViewModelFactory(db,config)
+            LoaCellViewModelFactory(db, config)
         )[LoaCellViewModel::class.java]
 
         setContent {
+            val selectedRoomId by loaCellViewModel.roomId.collectAsState()
+            BackHandler(selectedRoomId.isEmpty()) {
+                if (System.currentTimeMillis() - waitTime >= 1500) {
+                    waitTime = System.currentTimeMillis()
+                    loaCellViewModel.showSnackBar("뒤로가기 버튼을 한번 더 누르면 종료됩니다.")
+                } else {
+                    finish()
+                }
+            }
             MainContent(db, loaCellViewModel)
         }
     }
