e95b033723fb0df8bdafa0657838bcbf45ecbe8b
jmseb3 <jmseb3@naver.com>
2023-06-30 16:56:03 +0900

    update android 1.0.1(4)

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,8 +1,8 @@
 
 object AppConfig {
     const val AppName = "LoaCell"
-    const val versionCode = 3
-    const val version = "1.0.0"
+    const val versionCode = 4
+    const val version = "1.0.1"
     const val group = "com.wonddak"
     const val loaCellgroup = "$group.loacell"
     const val databaseName = "Database"
