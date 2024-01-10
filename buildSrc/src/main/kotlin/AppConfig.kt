aeb305dc10f5597ddd7e2ae17762eac58ce22fe2
jmseb3 <jmseb3@naver.com>
2024-01-10 21:14:15 +0900

    Release Android 1.1.2(9)

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,8 +1,8 @@
 
 object AppConfig {
     const val AppName = "LoaCell"
-    const val versionCode = 8
-    const val version = "1.1.1"
+    const val versionCode = 9
+    const val version = "1.1.2"
     const val group = "com.wonddak"
     const val loaCellgroup = "$group.loacell"
     const val databaseName = "Database"
