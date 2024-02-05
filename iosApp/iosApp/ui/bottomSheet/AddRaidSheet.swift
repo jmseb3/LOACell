63e2f114b7b581d454f29e6d6178548656351a10
jmseb3 <jmseb3@naver.com>
2024-02-05 23:18:39 +0900

    레이드 추가 화면 수정(1)

--- iosApp/iosApp/ui/bottomSheet/AddRaidSheet.swift
+++ iosApp/iosApp/ui/bottomSheet/AddRaidSheet.swift
@@ -86,7 +86,6 @@ private struct RaidSheetBase: View {
     let buttonAction :() -> Void
     
     @State private var titleText : String = ""
-    @State private var raidType : RaidType  = RaidType.etc
     @State private var abStart :Int = 1
     @State private var abEnd :Int = 1
     
@@ -97,7 +96,17 @@ private struct RaidSheetBase: View {
         (fbRaidInfo.type == RaidType.abrelshud) && (fbRaidInfo.difficulty != Difficulty.hell)
     }
     @State private var showDayUse :Bool = false
+    @State private var dayPick : Day = Day.none
     
+    private let days = Day.entries
+    
+    private var difList: [Difficulty]  {
+        if (Const().useExtreme) {
+            [Difficulty.normal,Difficulty.hard,Difficulty.hell,Difficulty.extremenormal,Difficulty.extremehard]
+        } else {
+            [Difficulty.normal,Difficulty.hard,Difficulty.hell]
+        }
+    }
     var body: some View {
         BaseSheet(
             title: title,
@@ -114,24 +123,6 @@ private struct RaidSheetBase: View {
                     placeHolder: "제목을 입력해주세요.",
                     text: $titleText
                 )
-                VStack {
-                    RaidSheetHeadeerText(text: "레이드 정보 선택")
-                    Divider()
-                    Picker("레이드 정보 선택",selection: $raidType) {
-                        ForEach(RaidType.entries, id:\.self) { item in
-                            Text(item.toKorString())
-                        }
-                    }
-                    .accentColor(.black)
-                }
-                VStack{
-                    RaidSheetHeadeerText(text: "난이도 선택")
-                    Divider()
-                    DifficultyRow(fbRaidInfo: $fbRaidInfo, difficultyList: [Difficulty.normal,Difficulty.hard,Difficulty.hell])
-                    if (Const().useExtreme) {
-                        DifficultyRow(fbRaidInfo: $fbRaidInfo, difficultyList: [Difficulty.extremenormal,Difficulty.extremehard])
-                    }
-                }
                 if showGateEdit {
                     VStack{
                         RaidSheetHeadeerText(text: "관문 선택")
@@ -164,86 +155,97 @@ private struct RaidSheetBase: View {
                     }
                     .animation(.spring, value : showGateEdit)
                 }
-                HStack {
-                    VStack {
-                        RaidSheetHeadeerText(text: "입장 레벨")
-                        Text(fbRaidInfo.getMinLevelText())
-                            .frame(maxWidth: .infinity)
-                            .frame(alignment: .leading)
+                Form {
+                    Section {
+                        Picker(
+                            "레이드",
+                            selection: Binding {
+                                fbRaidInfo.type
+                            } set: { type in
+                                fbRaidInfo = fbRaidInfo.updateType(type: type)
+                            }
+                        ) {
+                            ForEach(RaidType.entries, id:\.self) { item in
+                                Text(item.toKorString()).tag(item)
+                            }
+                        }
+                        .accentColor(.black)
+                        Picker(
+                            "난이도",
+                            selection: Binding {
+                                fbRaidInfo.difficulty
+                            } set: { dif in
+                                fbRaidInfo = fbRaidInfo.updateDifficulty(difficulty: dif)
+                            }
+                        ) {
+                            ForEach(difList, id:\.self) { item in
+                                let enabled = fbRaidInfo.difficultyEnabled(difficulty :item)
+                                if enabled {
+                                    Text(item.toKorString()).tag(item)
+                                }
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .accentColor(.black)
+                    } header: {
+                        Text("레이드 선택")
                     }
-                    VStack {
-                        RaidSheetHeadeerText(text: "입장 인원")
-                        Text(String(fbRaidInfo.type.maxPerson))
-                            .frame(maxWidth: .infinity)
-                            .frame(alignment: .leading)
-                    }
-                }
-                Spacer().frame(height: 10)
-                CheckButton(isOn: showDayUse, text: "일정 지정") {
-                    withAnimation {
-                        showDayUse = !showDayUse
-                    }
-                }
-                if showDayUse {
-                    let h :CGFloat = 100
-                    HStack {
+                    
+                    Section {
                         HStack {
-                            Picker("Choose a Hour", selection: $dayHour) {
+                            Text("입장 레벨")
+                            Spacer()
+                            Text(fbRaidInfo.getMinLevelText())
+                        }
+                        HStack {
+                            Text("입장 인원")
+                            Spacer()
+                            Text(String(fbRaidInfo.type.maxPerson))
+                        }
+                        
+                    } header: {
+                        Text("레이드 정보")
+                    }
+                    Button {
+                        withAnimation {
+                            showDayUse = !showDayUse
+                            if !showDayUse {
+                                dayPick = Day.none
+                            }
+                        }
+                    } label: {
+                        Text("일정 지정")
+                    }
+                    if showDayUse {
+                        Section {
+                            Picker("시", selection: $dayHour) {
                                 ForEach(Array(0...23), id: \.self) { hour in
                                     Text(String(format: "%02d", hour))
                                 }
                             }
-                            .frame(width:100, height: h)
-                            .pickerStyle(.wheel)
-                            .clipped()
-                            Text(":")
-                            Picker("Choose a Minute", selection: $dayMin) {
+                            Picker("분", selection: $dayMin) {
                                 ForEach(Array(0...59), id: \.self) { min in
                                     Text(String(format: "%02d", min))
                                 }
                             }
-                            .frame(width:100, height: h)
-                            .pickerStyle(.wheel)
-                            .clipped()
-                        }
-                        .cornerRadius(20)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(.black, lineWidth: 1)
-                        )
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .frame(height: h)
-                        VStack {
-                            let days = Day.entries
-                            HStack{
-                                ForEach(days[1...4],id:\.self) { day in
-                                    DayButton(day: day, selected: fbRaidInfo.day == day) {
-                                        updateDay(day: day)
-                                    }
+                            Picker(
+                                "요일",
+                                selection: $dayPick
+                            ) {
+                                ForEach(1 ..< days.count) {
+                                    Text(days[$0].text).tag(days[$0])
                                 }
                             }
-                            HStack{
-                                ForEach(days[5...7],id:\.self) { day in
-                                    DayButton(day: day, selected: fbRaidInfo.day == day) {
-                                        updateDay(day: day)
-                                    }
-                                }
-                                DayButton(day: Day.none, selected: false) {
-                                    
-                                }
-                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                        } header: {
+                            Text("레이드 일정 선택")
                         }
-                        .frame(height: h)
-                        .padding()
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                    }}
-                
+                    }
+                }
+                .frame(height: 350)
             }
             .onChange(of: titleText) { value in
                 fbRaidInfo = fbRaidInfo.updateTitle(title: value)
-            }
-            .onChange(of: raidType) { value in
-                fbRaidInfo = fbRaidInfo.updateType(type: value)
             }
             .onChange(of: dayMin) {value in
                 fbRaidInfo = fbRaidInfo.updateTimeMinute(minute : Int64(value))
@@ -251,8 +253,10 @@ private struct RaidSheetBase: View {
             .onChange(of: dayHour) {value in
                 fbRaidInfo = fbRaidInfo.updateTimeHour(hour : Int64(value))
             }
+            .onChange(of: dayPick) { value in
+                updateDay(day: value)
+            }
             .onAppear {
-                raidType = fbRaidInfo.type
                 titleText = fbRaidInfo.title
             }
         }
@@ -277,50 +281,5 @@ private struct RaidSheetHeadeerText : View {
                 .fontWeight(.semibold)
                 .frame(maxWidth: .infinity,alignment: .leading)
         }
-    }
-}
-
-private struct DifficultyRow : View {
-    @Binding var fbRaidInfo :FBRaidInfo
-    let difficultyList : [Difficulty]
-    var body: some View {
-        HStack {
-            ForEach(difficultyList,id:\.name) { difficulty in
-                let selected = fbRaidInfo.difficultySelected(difficulty :difficulty)
-                let enabled = fbRaidInfo.difficultyEnabled(difficulty :difficulty)
-                RadioButton(
-                    selected: selected,
-                    text: difficulty.toKorString(),
-                    enabled: enabled
-                )
-                {
-                    fbRaidInfo = fbRaidInfo.updateDifficulty(difficulty: difficulty)
-                }
-                .frame(maxWidth: .infinity)
-            }
-            
-        }.frame(maxWidth: .infinity)
-    }
-}
-
-
-private struct DayButton :View {
-    let day : Day
-    let selected :Bool
-    var update :() -> Void
-    var body: some View {
-        Button {
-            update()
-        } label: {
-            Text(day == Day.none ? "" : day.text)
-                .foregroundColor(.black)
-                .frame(maxWidth: .infinity,alignment: .center)
-                .padding(4)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.black, lineWidth: selected ? 1 : 0)
-                )
-        }
-        
     }
 }
