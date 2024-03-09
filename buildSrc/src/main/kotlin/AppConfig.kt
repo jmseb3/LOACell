1eb4626e9fcf43f0400a28448eba77fc49e45c53
jmseb3 <jmseb3@naver.com>
2024-03-09 21:06:03 +0900

    [AOS] 1.1.3(10)

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,8 +1,8 @@
 
 object AppConfig {
     const val AppName = "LoaCell"
-    const val versionCode = 9
-    const val version = "1.1.2"
+    const val versionCode = 10
+    const val version = "1.1.3"
     const val group = "com.wonddak"
     const val loaCellgroup = "$group.loacell"
     const val databaseName = "Database"
