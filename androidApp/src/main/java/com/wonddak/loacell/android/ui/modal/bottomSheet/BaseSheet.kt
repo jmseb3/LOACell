9c21d7b8231b7a09522eb29ad69740b6f51769b4
jmseb3 <jmseb3@naver.com>
2024-06-15 13:57:25 +0900

    IME Padding 추가

--- androidApp/src/main/java/com/wonddak/loacell/android/ui/modal/bottomSheet/BaseSheet.kt
+++ androidApp/src/main/java/com/wonddak/loacell/android/ui/modal/bottomSheet/BaseSheet.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Divider
@@ -54,7 +55,9 @@ fun BaseSheet(
     ModalBottomSheet(
         onDismissRequest = onDismissRequest,
         sheetState = sheetState,
-        dragHandle = null
+        dragHandle = null,
+        modifier = Modifier
+            .imePadding()
     ) {
         Column(
             modifier = Modifier
