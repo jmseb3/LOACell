0594e5fbb205bc3e6e745af17cc384b6d37affaf
jmseb3 <jmseb3@naver.com>
2026-07-29 15:29:30 +0900

    시스템 다크 모드 테마 적용

--- SharedUI/src/commonMain/kotlin/com.wonddak.loacell/theme/Theme.kt
+++ SharedUI/src/commonMain/kotlin/com.wonddak.loacell/theme/Theme.kt
@@ -102,8 +102,7 @@ internal fun AppTheme(
         val isDark by isDarkState
         SystemAppearance(!isDark)
         MaterialTheme(
-//            colorScheme = if (isDark) darkScheme else lightScheme,
-            colorScheme = lightScheme,
+            colorScheme = if (isDark) darkScheme else lightScheme,
             content = { Surface(content = content) }
         )
     }
