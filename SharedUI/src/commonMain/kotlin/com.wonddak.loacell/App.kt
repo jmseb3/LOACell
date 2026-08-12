393c6ee8d1f15018865b8ceee7f9c9f2981d105f
jmseb3 <jmseb3@naver.com>
2026-08-12 10:34:49 +0900

    이미지 디스크 캐시 크기 조정

--- SharedUI/src/commonMain/kotlin/com.wonddak.loacell/App.kt
+++ SharedUI/src/commonMain/kotlin/com.wonddak.loacell/App.kt
@@ -44,7 +44,7 @@ fun App(
             }
             .diskCache {
                 DiskCache.Builder().directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY / "image_cache")
-                    .maxSizeBytes(512L * 1024 * 1024) // 512MB
+                    .maxSizeBytes(128L * 1024 * 1024)
                     .build()
             }
             .build()
