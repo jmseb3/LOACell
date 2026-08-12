89d2d52ecb16c36dfb544c1c094780bfd0f1968e
jmseb3 <jmseb3@naver.com>
2026-08-12 16:36:16 +0900

    feat(firebase): Crashlytics 통합
    - `Package.swift`에 Firebase Crashlytics 패키지 의존성 추가
    - Crashlytics dSYM 업로드를 위한 빌드 스크립트 추가
    - 디버그 정보 포맷을 `dwarf-with-dsym`으로 설정

--- iosApp/KotlinMultiplatformLinkedPackage/Package.swift
+++ iosApp/KotlinMultiplatformLinkedPackage/Package.swift
@@ -35,6 +35,10 @@ let package = Package(
           package: "firebase-ios-sdk"
         ),
         .product(
+          name: "FirebaseCrashlytics",
+          package: "firebase-ios-sdk"
+        ),
+        .product(
           name: "FirebaseFirestore",
           package: "firebase-ios-sdk"
         ),
