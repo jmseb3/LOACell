b54bfe3369ab7128f1a78124d8ae26f0b4059a1d
jmseb3 <jmseb3@naver.com>
2026-08-12 10:37:58 +0900

    구글 로그인 버튼 디자인 적용

--- SharedUI/src/commonMain/kotlin/com.wonddak.loacell/ui/login/GoogleSignInButton.kt
+++ SharedUI/src/commonMain/kotlin/com.wonddak.loacell/ui/login/GoogleSignInButton.kt
@@ -1,15 +1,23 @@
 package com.wonddak.loacell.ui.login
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.wonddak.hellogin.core.TokenResultHandler
 import com.wonddak.hellogin.google.GoogleResult
 import com.wonddak.loacell.auth.LoginHelper
 import kotlinx.coroutines.launch
+import loacell.sharedui.generated.resources.Res
+import loacell.sharedui.generated.resources.button_google
+import loacell.sharedui.generated.resources.logo_google
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun GoogleSignInButton(
@@ -23,16 +31,24 @@ fun GoogleSignInButton(
         scope.launch {
             loginHelper.requestGoogleLogin(tokenResultHandler)
         }
-        Unit
     }
 
     if (iconOnly) {
         IconButton(onClick = onClick, modifier = modifier) {
-            Text("G")
+            Image(
+                painter = painterResource(Res.drawable.logo_google),
+                contentDescription = "Google login",
+                modifier = Modifier.size(40.dp),
+            )
         }
     } else {
-        Button(onClick = onClick, modifier = modifier) {
-            Text("Google로 로그인")
-        }
+        Image(
+            painter = painterResource(Res.drawable.button_google),
+            contentDescription = "Google login",
+            contentScale = ContentScale.FillBounds,
+            modifier = modifier
+                .aspectRatio(352f / 63f)
+                .clickable(onClick = onClick),
+        )
     }
 }
